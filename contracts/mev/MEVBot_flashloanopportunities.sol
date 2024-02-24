11a67d272bbe531b5d066bf55459fc13e93719e6
00hello <445986+00hello@users.noreply.github.com>
2024-02-24 13:34:57 -0500

    Create new interface

--- contracts/mev/MEVBot_flashloanopportunities.sol
+++ contracts/mev/MEVBot_flashloanopportunities.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
  * @title MEVBot
  * @dev Experimental MEV bot for flashloan opportunities
  * @author Your Name
- * @notice Created on 2024-02-03
+ * @notice Created on 2024-02-24
  */
 interface IUniswapV2Pair {
     function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
@@ -44,8 +44,9 @@ contract MEVBot {
     }
     
     // Strategy for private transaction pools
-    function sandwichTrade(address pair, uint256 amount) external onlyOwner {
+    function calculateInterest(address user) public view returns (uint256) {
         // Implementation here
+        return 0;
     }
     
     receive() external payable {}
