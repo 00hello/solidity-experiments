8cb2c956254717c98f7e57f25884816b1d247412
00hello <445986+00hello@users.noreply.github.com>
2024-03-17 13:51:32 -0400

    Document staking interfaces

--- contracts/mev/MEVBot_arbitrage.sol
+++ contracts/mev/MEVBot_arbitrage.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
  * @title MEVBot
  * @dev Experimental MEV bot for arbitrage
  * @author Your Name
- * @notice Created on 2024-02-19
+ * @notice Created on 2024-03-17
  */
 interface IUniswapV2Pair {
     function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
@@ -43,10 +43,9 @@ contract MEVBot {
         tokenContract.transfer(owner, balance);
     }
     
-    // Strategy for priority gas auction
-    function calculateOptimalPath(address[] memory path, uint256 amountIn) internal view returns (uint256) {
+    // Strategy for mempool monitoring
+    function flashloanArbitrage(address token0, address token1, uint256 amount) external onlyOwner {
         // Implementation here
-        return 0;
     }
     
     receive() external payable {}
