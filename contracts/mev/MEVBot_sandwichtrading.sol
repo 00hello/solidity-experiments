5b21f80b8ebb6d1327b8724ff75300c5240e3b44
00hello <445986+00hello@users.noreply.github.com>
2024-08-13 09:30:00 -0400

    Create fuzzing tests for AI integration

--- contracts/mev/MEVBot_sandwichtrading.sol
+++ contracts/mev/MEVBot_sandwichtrading.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
  * @title MEVBot
  * @dev Experimental MEV bot for sandwich trading
  * @author Your Name
- * @notice Created on 2024-07-30
+ * @notice Created on 2024-08-13
  */
 interface IUniswapV2Pair {
     function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
@@ -44,8 +44,9 @@ contract MEVBot {
     }
     
     // Strategy for private transaction pools
-    function sandwichTrade(address pair, uint256 amount) external onlyOwner {
+    function calculateOptimalPath(address[] memory path, uint256 amountIn) internal view returns (uint256) {
         // Implementation here
+        return 0;
     }
     
     receive() external payable {}
