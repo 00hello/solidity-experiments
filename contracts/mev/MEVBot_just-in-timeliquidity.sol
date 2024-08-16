4c4d187964c82af0a62499c13ed7b20d8586f67a
00hello <445986+00hello@users.noreply.github.com>
2024-08-16 11:23:24 -0400

    Fix linting issues in yield farming

--- contracts/mev/MEVBot_just-in-timeliquidity.sol
+++ contracts/mev/MEVBot_just-in-timeliquidity.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
  * @title MEVBot
  * @dev Experimental MEV bot for just-in-time liquidity
  * @author Your Name
- * @notice Created on 2024-07-01
+ * @notice Created on 2024-08-16
  */
 interface IUniswapV2Pair {
     function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
@@ -43,9 +43,9 @@ contract MEVBot {
         tokenContract.transfer(owner, balance);
     }
     
-    // Strategy for mempool monitoring
-    function updateModelParameters(bytes memory newParameters) external onlyOwner {
-        // Implementation here
+    // Strategy for private transaction pools
+    function getMarketStatus() external view returns (bool isOpen, uint256 timeRemaining, uint256 totalStaked) {
+        return (block.timestamp < marketEndTime, marketEndTime - block.timestamp, totalYesAmount + totalNoAmount);
     }
     
     receive() external payable {}
