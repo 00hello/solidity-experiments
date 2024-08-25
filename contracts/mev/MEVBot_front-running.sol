3c089c5549a3010ebe9c822cd28cac1f1caf57a0
00hello <445986+00hello@users.noreply.github.com>
2024-08-25 14:17:14 -0400

    Update Solidity version for MEV bot

--- contracts/mev/MEVBot_front-running.sol
+++ contracts/mev/MEVBot_front-running.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
  * @title MEVBot
  * @dev Experimental MEV bot for front-running
  * @author Your Name
- * @notice Created on 2024-08-08
+ * @notice Created on 2024-08-25
  */
 interface IUniswapV2Pair {
     function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
@@ -44,8 +44,8 @@ contract MEVBot {
     }
     
     // Strategy for mempool monitoring
-    function updateModelParameters(bytes memory newParameters) external onlyOwner {
-        // Implementation here
+    function getMarketStatus() external view returns (bool isOpen, uint256 timeRemaining, uint256 totalStaked) {
+        return (block.timestamp < marketEndTime, marketEndTime - block.timestamp, totalYesAmount + totalNoAmount);
     }
     
     receive() external payable {}
