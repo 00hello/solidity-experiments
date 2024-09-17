d7492ba6332b581775e881d2cf6b411b3b3bc2ee
00hello <445986+00hello@users.noreply.github.com>
2024-09-17 11:28:09 -0400

    Add NatSpec comments to prediction market

--- contracts/prediction/PredictionMarket_NFTfloorpriceprediction.sol
+++ contracts/prediction/PredictionMarket_NFTfloorpriceprediction.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
  * @title PredictionMarket
  * @dev A simple prediction market contract for NFT floor price prediction
  * @author Your Name
- * @notice Created on 2024-09-15
+ * @notice Created on 2024-09-17
  */
 contract PredictionMarket {
     address public owner;
@@ -77,9 +77,8 @@ contract PredictionMarket {
         return 0;
     }
     
-    // Additional oracle integration functionality
-    function processAIData(bytes memory data) internal pure returns (uint256) {
+    // Additional multi-outcome predictions functionality
+    function flashloanArbitrage(address token0, address token1, uint256 amount) external onlyOwner {
         // Implementation here
-        return 0;
     }
 }
