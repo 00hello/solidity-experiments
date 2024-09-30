3a03eff5f74d75f79f3293ad16b81110c61acad8
00hello <445986+00hello@users.noreply.github.com>
2024-09-30 10:57:24 -0400

    Update documentation for staking

--- contracts/prediction/PredictionMarket_DeFiprotocolTVLprediction.sol
+++ contracts/prediction/PredictionMarket_DeFiprotocolTVLprediction.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
  * @title PredictionMarket
  * @dev A simple prediction market contract for DeFi protocol TVL prediction
  * @author Your Name
- * @notice Created on 2024-09-20
+ * @notice Created on 2024-09-30
  */
 contract PredictionMarket {
     address public owner;
@@ -77,9 +77,8 @@ contract PredictionMarket {
         return 0;
     }
     
-    // Additional automated market making functionality
-    function calculateInterest(address user) public view returns (uint256) {
+    // Additional insurance against incorrect outcomes functionality
+    function updateModelParameters(bytes memory newParameters) external onlyOwner {
         // Implementation here
-        return 0;
     }
 }
