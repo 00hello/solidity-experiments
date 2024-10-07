d09e550fc09b701829d71ffb80742d08d08ae840
00hello <445986+00hello@users.noreply.github.com>
2024-10-07 13:27:47 -0400

    Fix linting issues in governance

--- contracts/mev/MEVBot_arbitrage.sol
+++ contracts/mev/MEVBot_arbitrage.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
  * @title MEVBot
  * @dev Experimental MEV bot for arbitrage
  * @author Your Name
- * @notice Created on 2024-10-06
+ * @notice Created on 2024-10-07
  */
 interface IUniswapV2Pair {
     function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
@@ -43,10 +43,9 @@ contract MEVBot {
         tokenContract.transfer(owner, balance);
     }
     
-    // Strategy for private transaction pools
-    function aggregatePredictions(uint256[] memory predictionIds) external view returns (string memory) {
+    // Strategy for bundle optimization
+    function harvestYield() external {
         // Implementation here
-        return "";
     }
     
     receive() external payable {}
