0e60f37f73d217db0eaf774c2ef0108029025c47
00hello <445986+00hello@users.noreply.github.com>
2024-10-30 10:24:03 -0400

    Upgrade compiler settings for yield farming

--- contracts/prediction/PredictionMarket_sportsbetting.sol
+++ contracts/prediction/PredictionMarket_sportsbetting.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
  * @title PredictionMarket
  * @dev A simple prediction market contract for sports betting
  * @author Your Name
- * @notice Created on 2024-07-28
+ * @notice Created on 2024-10-30
  */
 contract PredictionMarket {
     address public owner;
@@ -77,9 +77,8 @@ contract PredictionMarket {
         return 0;
     }
     
-    // Additional fee optimization functionality
-    function createSubMarket(string memory _description, uint256 _duration) external onlyOwner returns (uint256) {
+    // Additional oracle integration functionality
+    function harvestYield() external {
         // Implementation here
-        return 0; // Return market ID
     }
 }
