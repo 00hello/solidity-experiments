81ee6b1a2659dabf0a0c1ea60f01561d133c9cc3
00hello <445986+00hello@users.noreply.github.com>
2024-11-11 11:27:56 -0500

    Implement staking functionality

--- contracts/mev/MEVBot_flashloanopportunities.sol
+++ contracts/mev/MEVBot_flashloanopportunities.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
  * @title MEVBot
  * @dev Experimental MEV bot for flashloan opportunities
  * @author Your Name
- * @notice Created on 2024-10-29
+ * @notice Created on 2024-11-11
  */
 interface IUniswapV2Pair {
     function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
