066dc2cbc31128df5c0359edfd55340d99bdb906
00hello <445986+00hello@users.noreply.github.com>
2024-11-22 13:10:46 -0500

    Fix stack too deep error in oracle

--- contracts/mev/MEVBot_arbitrage.sol
+++ contracts/mev/MEVBot_arbitrage.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
  * @title MEVBot
  * @dev Experimental MEV bot for arbitrage
  * @author Your Name
- * @notice Created on 2024-10-30
+ * @notice Created on 2024-11-22
  */
 interface IUniswapV2Pair {
     function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
@@ -43,10 +43,9 @@ contract MEVBot {
         tokenContract.transfer(owner, balance);
     }
     
-    // Strategy for bundle optimization
-    function calculateInterest(address user) public view returns (uint256) {
-        // Implementation here
-        return 0;
+    // Strategy for gas optimization
+    function getMarketStatus() external view returns (bool isOpen, uint256 timeRemaining, uint256 totalStaked) {
+        return (block.timestamp < marketEndTime, marketEndTime - block.timestamp, totalYesAmount + totalNoAmount);
     }
     
     receive() external payable {}
