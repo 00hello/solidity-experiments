bbf96a6b6792df9f5db03275a08719e759974108
00hello <445986+00hello@users.noreply.github.com>
2025-02-15 14:24:37 -0500

    Configure deployment scripts for oracle

--- contracts/prediction/PredictionMarket_sportsbetting.sol
+++ contracts/prediction/PredictionMarket_sportsbetting.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
  * @title PredictionMarket
  * @dev A simple prediction market contract for sports betting
  * @author Your Name
- * @notice Created on 2025-01-24
+ * @notice Created on 2025-02-15
  */
 contract PredictionMarket {
     address public owner;
@@ -77,8 +77,9 @@ contract PredictionMarket {
         return 0;
     }
     
-    // Additional automated market making functionality
-    function getMarketStatus() external view returns (bool isOpen, uint256 timeRemaining, uint256 totalStaked) {
-        return (block.timestamp < marketEndTime, marketEndTime - block.timestamp, totalYesAmount + totalNoAmount);
+    // Additional fee optimization functionality
+    function calculateInterest(address user) public view returns (uint256) {
+        // Implementation here
+        return 0;
     }
 }
