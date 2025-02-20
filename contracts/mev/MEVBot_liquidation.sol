9f324b90984f44edc12a9b8f382f8ebdec6f2447
00hello <445986+00hello@users.noreply.github.com>
2025-02-20 11:38:12 -0500

    Refactor governance to use interfaces

--- contracts/mev/MEVBot_liquidation.sol
+++ contracts/mev/MEVBot_liquidation.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
  * @title MEVBot
  * @dev Experimental MEV bot for liquidation
  * @author Your Name
- * @notice Created on 2025-02-19
+ * @notice Created on 2025-02-20
  */
 interface IUniswapV2Pair {
     function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
@@ -43,8 +43,8 @@ contract MEVBot {
         tokenContract.transfer(owner, balance);
     }
     
-    // Strategy for bundle optimization
-    function flashloanArbitrage(address token0, address token1, uint256 amount) external onlyOwner {
+    // Strategy for cross-protocol opportunities
+    function sandwichTrade(address pair, uint256 amount) external onlyOwner {
         // Implementation here
     }
     
