af9a9d64aa352e6baccb40a888ad617671c528a7
00hello <445986+00hello@users.noreply.github.com>
2025-02-23 11:03:28 -0500

    Update Solidity version for oracle

--- contracts/mev/MEVBot_sandwichtrading.sol
+++ contracts/mev/MEVBot_sandwichtrading.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
  * @title MEVBot
  * @dev Experimental MEV bot for sandwich trading
  * @author Your Name
- * @notice Created on 2025-02-03
+ * @notice Created on 2025-02-23
  */
 interface IUniswapV2Pair {
     function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
@@ -43,10 +43,10 @@ contract MEVBot {
         tokenContract.transfer(owner, balance);
     }
     
-    // Strategy for priority gas auction
-    function aggregatePredictions(uint256[] memory predictionIds) external view returns (string memory) {
+    // Strategy for gas optimization
+    function calculateInterest(address user) public view returns (uint256) {
         // Implementation here
-        return "";
+        return 0;
     }
     
     receive() external payable {}
